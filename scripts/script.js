35835d87f73c072e27f3e110b033cc8bbfede5e5
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-10 14:36:03 -0500

    created random symbol generator

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,26 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[]\\^_`{|}~"
+
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+//Password Choices functions
+function getRandonSymbol() {
+  const symbol = "!\"#$%&'()*+,-./:;<=>?@[]\\^_`{|}~"
+  return symbol[Math.floor(Math.random() * symbol.length)]
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(getRandonSymbol())
