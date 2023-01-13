741019ae49993222df2ed84bc7e86c4ca5b4d780
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-13 10:42:21 -0500

    Added lowerCase, number and symbol variables

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,18 @@
 // Assignment code here
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+// Array of selections to be included in password
+var upperCaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
 ];
 
+var lowerCaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+]
 
+var numberCharacters = ['0','1','2','3','4','5','6','7','8','9','@'
+
+]
+
+var symbolCharacters = ['!','#','$','%','&','(',',',')','*','+',',','-','.','/',',:',';','<','=','>','?','@','[',']','^','_','{','|','}','~'
+
+]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
