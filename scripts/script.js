f009fc72abab30b2d65841fc75baf773c3e00263
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-13 15:04:08 -0500

    added get random upper case function

--- scripts/script.js
+++ scripts/script.js
@@ -13,22 +13,33 @@ var numberCharacters = ['0','1','2','3','4','5','6','7','8','9','@'
 var symbolCharacters = ['!','#','$','%','&','(',',',')','*','+',',','-','.','/',',:',';','<','=','>','?','@','[',']','^','_','{','|','}','~'
 
 ]
+// Functions to generate choices
 
+function getRandomUpperCase () {
+  var upperCaseChoice = window.confirm('Would you like to use lower case letters in your password?')
+  console.log(upperCaseChoice);
+  }
+  
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 function generatePassword () {
-  console.log("Button clicked")
+  console.log('Button clicked')
 
 // 1. Prompt for password criteria
 //  a. Password Length 8 < 128
-  var lengthChoice = window.prompt("Please select enter your password length between 8 and 128 characters")
+  var lengthChoice = window.prompt('Please select enter your password length between 8 and 128 characters')
   console.log(lengthChoice);
   if (lengthChoice > 7 && lengthChoice < 129){
-    window.confirm("You have selected " + lengthChoice + "Character Length")}
+    window.confirm('You have selected ' + lengthChoice + ' Character Length')}
     else {
-      window.alert()
-    }
+      window.alert('Password must be between 8 and 128 characters');
+      generatePassword();
+
+    getRandomUpperCase();
+  
+  }
+
     var passwordChoices = {
       lengthChoice: lengthChoice,
 
